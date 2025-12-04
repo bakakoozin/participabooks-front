f89dcdbcf60b7fa31418220d6397417128a1f5c9
bakakoozin <cyrilpebre@gmail.com>
2025-12-04 23:21:23 +0100

    test vite config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import process from 'process'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import process from "process";
 
 export default ({ mode }) => {
   // charge .env, .env.production, etc + process.env
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  console.log("VITE_API_URL:", env.VITE_API_URL);
 
   return defineConfig({
     plugins: [react()],
     define: {
-      // ou re-map une VITE si besoin
-      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
-      'import.meta.env.VITE_BASE_URL_MEDIAS': JSON.stringify(env.VITE_BASE_URL_MEDIAS),
+      "process.env": {
+        ...env, //merge all env
+      },
     },
-  })
-}
+  });
+};
