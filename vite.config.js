a53e9e56bacb10c3a4639f980c4fc080f88dd3d0
bakakoozin <cyrilpebre@gmail.com>
2025-12-04 23:14:30 +0100

    modif vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import process from 'process'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  // charge .env, .env.production, etc + process.env
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return defineConfig({
+    plugins: [react()],
+    define: {
+      // ou re-map une VITE si besoin
+      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
+      'import.meta.env.VITE_BASE_URL_MEDIAS': JSON.stringify(env.VITE_BASE_URL_MEDIAS),
+    },
+  })
+}
