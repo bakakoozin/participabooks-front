db5b7bfd37eca2c062950ae2b991e8a23f9fca71
bakakoozin <cyrilpebre@gmail.com>
2025-12-26 14:38:32 +0100

    variables pour passage en tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import process from "process";
 
@@ -7,7 +8,7 @@ export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "VITE_");
 
   return defineConfig({
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     define: {
       "process.env": {
         ...env, //merge all env
